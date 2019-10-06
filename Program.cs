ff8c18dc5f646f8efe4cec17efe5883b376911fe
ShiningLea <49339966+AnErrupTion@users.noreply.github.com>
2019-10-06 09:17:21 +0200

    3.3

--- Program.cs
+++ Program.cs
@@ -8,10 +8,13 @@ namespace SiteBotnet3
     class Program
     {
         private static readonly string AppName = "SiteBotnet";
-        private static readonly string AppVersion = "3.2";
+        private static readonly string AppVersion = "3.3";
 
         static void Main(string[] args)
         {
+            if (!File.Exists("proxies.txt")) File.WriteAllText("proxies.txt", "");
+
+            string Method;
             string Target;
             int Threads;
             string ProxyType;
@@ -20,23 +23,25 @@ namespace SiteBotnet3
             if (args.Length == 0)
             {
                 Parser p = new Parser("config.txt");
+                Method = p.ReadConfig("Method").ToString().Split('=')[1].Replace(" ", "");
                 Target = p.ReadConfig("Target").ToString().Split('=')[1].Replace(" ", "");
                 Threads = Convert.ToInt32(p.ReadConfig("Threads").ToString().Split('=')[1].Replace(" ", ""));
                 ProxyType = p.ReadConfig("ProxyType").ToString().Split('=')[1].Replace(" ", "");
             }
             else
             {
-                Target = args[0];
-                Threads = Convert.ToInt32(args[1]);
-                ProxyType = args[2];
+                Method = args[0];
+                Target = args[1];
+                Threads = Convert.ToInt32(args[2]);
+                ProxyType = args[3];
             }
 
-            if (!File.Exists("proxies.txt")) File.WriteAllText("proxies.txt", "");
             Console.Title = AppName + " v" + AppVersion;
 
             for (int i = 0; i < Threads; i++)
             {
                 Requests req = new Requests("[ThreadHttp-" + i + "]");
+                req.SetMethod(Method);
                 req.SetTarget(Target);
                 req.SetThreads(Threads);
                 req.SetProxyType(ProxyType);
